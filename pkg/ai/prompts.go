c41ba0095885c7aeb46382901ac122d3b12e0b78
yuehuazhang <yuehuazhang@tencent.com>
2023-12-03 23:20:11 +0800

    feat:ianvs

--- pkg/ai/prompts.go
+++ pkg/ai/prompts.go
@@ -1,10 +1,10 @@
 package ai
 
 const (
-	default_prompt = `Simplify the following Kubernetes error message delimited by triple dashes written in --- %s --- language; --- %s ---.
-	Provide the most possible solution in a step by step style in no more than 280 characters. Write the output in the following format:
-	Error: {Explain error here}
-	Solution: {Step by step solution here}
+	default_prompt = `简化以下由三重破折号分隔的 Kubernetes 错误消息，该消息以 --- %s --- 语言编写； --- %s ---。
+	以不超过 280 个字符的分步方式提供最可能的解决方案。 按以下格式编写输出：
+	错误：{在此解释错误}
+	解决方案：{此处为逐步解决方案}
 	`
 	trivy_vuln_prompt = "Explain the following trivy scan result and the detail risk or root cause of the CVE ID, then provide a solution. Response in %s: %s"
 	trivy_conf_prompt = "Explain the following trivy scan result and the detail risk or root cause of the security check, then provide a solution."
